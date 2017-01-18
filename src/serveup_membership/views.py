bea50dc00c08d11a8016fef0e92c5027a35128d8
aminhp93 <minhpn.org.ec@gmail.com>
2017-01-17 20:27:48 -0800

    embed from youtube

--- src/serveup_membership/views.py
+++ src/serveup_membership/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from videos.models import Video
 
 
 def home(request):
-	context = {}
+	videos = Video.objects.all()
+	embeds = []
+
+	for video in videos:
+		embeds.append("{}".format(video.embed_code))
+
+	context = {
+		"videos": videos,
+		"number": videos.count(),
+		"embeds": embeds,
+	}
 	return render(request, "home.html", context)
+
