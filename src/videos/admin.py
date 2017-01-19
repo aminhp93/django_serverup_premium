b08cee2492ee16594ddc22e076094d40d06bd1b0
aminhp93 <minhpn.org.ec@gmail.com>
2017-01-18 19:30:22 -0800

    using signals to create slugs

--- src/videos/admin.py
+++ src/videos/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import Video, Category
 # Register your models here.
 
-admin.site.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+	list_display = ["__str__", "slug"]
+	# fields = ["title"]
+	prepopulated_fields = {'slug': ('title',)}
+	class Meta:
+		model = Video
+
+admin.site.register(Video, VideoAdmin)
 admin.site.register(Category)
